4060cdd9367e46d01112fb6676bbf7429b148ef0
Julio <Cocoletzi>
2021-04-22 13:59:27 -0500

    [FIX]: Se arreglaron problemas causados por commits

--- LoginForms/WhatsApp.Designer.cs
+++ LoginForms/WhatsApp.Designer.cs
@@ -29,172 +29,170 @@ namespace LoginForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.cmbConnector = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.cmbConnector = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEnviar = new System.Windows.Forms.Button();
             this.rtxtResponseMessage = new System.Windows.Forms.RichTextBox();
             this.rtxtSendMessage = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WhatsApp connector";
             // 
             // cmbConnector
             // 
             this.cmbConnector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbConnector.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbConnector.FormattingEnabled = true;
-            this.cmbConnector.Location = new System.Drawing.Point(18, 46);
-            this.cmbConnector.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbConnector.Location = new System.Drawing.Point(12, 30);
             this.cmbConnector.Name = "cmbConnector";
-            this.cmbConnector.Size = new System.Drawing.Size(966, 33);
-            this.cmbConnector.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(15, 14);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(234, 29);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "WhatsApp connector";
+            this.cmbConnector.Size = new System.Drawing.Size(645, 24);
+            this.cmbConnector.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(18, 106);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(12, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(518, 29);
-            this.label2.TabIndex = 3;
+            this.label2.Size = new System.Drawing.Size(319, 18);
+            this.label2.TabIndex = 5;
             this.label2.Text = "Telefono cliente formato: país + área + telefono";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(20, 160);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(964, 30);
-            this.textBox1.TabIndex = 4;
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnEnviar);
             this.panel1.Controls.Add(this.rtxtResponseMessage);
             this.panel1.Controls.Add(this.rtxtSendMessage);
+            this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Location = new System.Drawing.Point(18, 203);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Location = new System.Drawing.Point(12, 132);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(966, 579);
-            this.panel1.TabIndex = 5;
+            this.panel1.Size = new System.Drawing.Size(645, 377);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(403, 563);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 9;
+            this.btnEnviar.Text = "Enviar Mensaje";
+            this.btnEnviar.UseVisualStyleBackColor = true;
             // 
             // rtxtResponseMessage
             // 
             this.rtxtResponseMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.rtxtResponseMessage.Location = new System.Drawing.Point(325, 31);
+            this.rtxtResponseMessage.Location = new System.Drawing.Point(323, 31);
             this.rtxtResponseMessage.Name = "rtxtResponseMessage";
             this.rtxtResponseMessage.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
             this.rtxtResponseMessage.Size = new System.Drawing.Size(279, 329);
-            this.rtxtResponseMessage.TabIndex = 2;
+            this.rtxtResponseMessage.TabIndex = 5;
             this.rtxtResponseMessage.Text = "";
-            this.rtxtResponseMessage.TextChanged += new System.EventHandler(this.rtxtResponseMessage_TextChanged);
-=======
-            this.rtxtResponseMessage.Location = new System.Drawing.Point(488, 48);
-            this.rtxtResponseMessage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.rtxtResponseMessage.Name = "rtxtResponseMessage";
-            this.rtxtResponseMessage.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
-            this.rtxtResponseMessage.Size = new System.Drawing.Size(416, 504);
-            this.rtxtResponseMessage.TabIndex = 2;
-            this.rtxtResponseMessage.Text = "";
->>>>>>> 885d3e1ce8e521142206e01136cb5dee6f567bd5
             // 
             // rtxtSendMessage
             // 
             this.rtxtSendMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtxtSendMessage.Location = new System.Drawing.Point(9, 48);
-            this.rtxtSendMessage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rtxtSendMessage.Location = new System.Drawing.Point(6, 31);
             this.rtxtSendMessage.Name = "rtxtSendMessage";
             this.rtxtSendMessage.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
-            this.rtxtSendMessage.Size = new System.Drawing.Size(416, 504);
-            this.rtxtSendMessage.TabIndex = 1;
+            this.rtxtSendMessage.Size = new System.Drawing.Size(279, 329);
+            this.rtxtSendMessage.TabIndex = 4;
             this.rtxtSendMessage.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mensaje - emoticones: win + .";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(4, 18);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(3, 12);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(271, 25);
+            this.label3.Size = new System.Drawing.Size(181, 16);
             this.label3.TabIndex = 0;
             this.label3.Text = "Mensaje - emoticones: win + .";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(644, 22);
+            this.textBox1.TabIndex = 7;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(18, 792);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Location = new System.Drawing.Point(12, 515);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(222, 24);
-            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Size = new System.Drawing.Size(157, 17);
+            this.checkBox1.TabIndex = 8;
             this.checkBox1.Text = "Activar interaccion vía chat";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnClose
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(183, 866);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 35);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Cerrar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(122, 563);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
             // 
-            // btnEnviar
+            // btnSend
             // 
-            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEnviar.Location = new System.Drawing.Point(604, 866);
-            this.btnEnviar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnEnviar.Name = "btnEnviar";
-            this.btnEnviar.Size = new System.Drawing.Size(112, 35);
-            this.btnEnviar.TabIndex = 8;
-            this.btnEnviar.Text = "Enviar Mensaje";
-            this.btnEnviar.UseVisualStyleBackColor = true;
-<<<<<<< HEAD
-            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
-=======
->>>>>>> 885d3e1ce8e521142206e01136cb5dee6f567bd5
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(403, 563);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 10;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
-            // WhatsApp
+            // WhatsApp1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 952);
-            this.Controls.Add(this.btnEnviar);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(670, 604);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnClose);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.cmbConnector);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Name = "WhatsApp";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "WhatsApp";
-            this.Load += new System.EventHandler(this.WhatsApp_Load);
+            this.Controls.Add(this.label1);
+            this.Name = "WhatsApp1";
+            this.Text = "WhatsApp1";
+            this.Load += new System.EventHandler(this.WhatsApp1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -204,16 +202,18 @@ namespace LoginForms
 
         #endregion
 
-        private System.Windows.Forms.ComboBox cmbConnector;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbConnector;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.RichTextBox rtxtSendMessage;
         private System.Windows.Forms.RichTextBox rtxtResponseMessage;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnSend;
     }
 }
--- LoginForms/WhatsApp.cs
+++ LoginForms/WhatsApp.cs
@@ -18,24 +18,15 @@ namespace LoginForms
             InitializeComponent();
         }
 
-        private void WhatsApp_Load(object sender, EventArgs e)
+        private void WhatsApp1_Load(object sender, EventArgs e)
         {
-<<<<<<< HEAD
             Task task = new Task(client.Connect);
             task.Start();
         }
 
-        private void btnEnviar_Click(object sender, EventArgs e)
+        private void btnSend_Click(object sender, EventArgs e)
         {
 
-        }
-
-        private void rtxtResponseMessage_TextChanged(object sender, EventArgs e)
-        {
-
-=======
-            Console.WriteLine("hello mdfcks");
->>>>>>> 885d3e1ce8e521142206e01136cb5dee6f567bd5
         }
     }
 }
