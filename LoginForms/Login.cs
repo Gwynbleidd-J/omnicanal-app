4baf802007e4c4cc85e4d62ba12458ef971400e0
Julio Cocoletzi <cocoletzi1997@gmail.com>
2022-01-09 18:16:16 -0600

    [FIX]: se arreglaron problemas en login.cs y app.cs

--- LoginForms/Login.cs
+++ LoginForms/Login.cs
@@ -76,7 +76,7 @@ namespace LoginForms
 
         private async void userLogin(IProgress<int> progress = null)
         {
-            string ipAddress = rh.GetLocalIpAddress();
+            string ipAddress = rh.GetPublicIpAddress();
 
             try
             {
